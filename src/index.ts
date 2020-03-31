b1b11bfff3b3dc6a555d0d441f023ae46062a8ee
Utkarsh Shigihalli <onlyutkarsh@users.noreply.github.com>
2020-03-31 21:34:47 +0100

    fix imports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import core from "@actions/core";
-import glob from "@actions/glob";
+import * as core from "@actions/core";
+import * as glob from "@actions/glob";
 import { patchFiles, globFiles } from "./patchfiles";
 
 async function run() {
