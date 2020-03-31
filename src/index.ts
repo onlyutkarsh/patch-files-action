fbdc3cc76e5d15305bb5213f28f1ea7cec643823
Utkarsh Shigihalli <onlyutkarsh@users.noreply.github.com>
2020-03-31 21:31:50 +0100

    sync processing

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,8 @@ async function run() {
     try {
         let pattern = core.getInput("files", { required: true });
         let globber = await globFiles(pattern);
-        for await (const file of globber.globGenerator()) {
-            core.info(file);
-        }
+        let files = await globber.glob();
+        core.info(JSON.stringify(files));
     } catch (error) {
         core.setFailed(error.message);
     }
